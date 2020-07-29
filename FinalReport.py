1e247e6c527f8f2dd49fc2aed4b9a26ea8e3785d
Akhil Jha <akjha@redhat.com>
2020-07-29 23:51:08 +0530

    Initial commit

---
+++ FinalReport.py
@@ -0,0 +1,57 @@
+import openpyxl as op
+from openpyxl.styles import Font
+from os.path import exists
+
+
+def common_keywords(cal_val):
+    data.cell(row=1, column=cal_val + 3).font = Font(bold=True)
+    if cal_val == 3:
+        data.cell(row=1, column=cal_val + 3).value = "Component based on problem statement"
+    elif cal_val == 4:
+        data.cell(row=1, column=cal_val + 3).value = "Component based on Original statement"
+    for i in range(2, total_data_rows + 1):
+        problem_description = data.cell(row=i, column=cal_val).value.lower()
+        temp = []
+        for keyword in keywords:
+            if keyword in problem_description:
+                temp.append(keyword)
+        temp = " ".join(temp)
+        data.cell(row=i, column=cal_val + 3).value = temp
+    content.save(file)
+
+
+def file_exists(file):
+    if not exists(file):
+        print('\nFile not found\nCheck for file path or file name.\n')
+        exit(1)
+    else:
+        return True
+
+
+file = input("\nEnter excel filename: ")
+if 'xlsx' in file:
+    file_exists(file)
+else:
+    file = file + '.xlsx'
+    file_exists(file)
+
+
+data_sheet_name = input("\nEnter sheet name: ")
+component_sheet_name = input("\nEnter sheet name with components: ")
+
+print("\nIn progress...\n")
+content = op.load_workbook(file)
+
+data = content[data_sheet_name]
+components = content[component_sheet_name]
+
+total_data_rows = data.max_row
+total_keyword_rows = components.max_row
+
+keywords = [components.cell(row=i, column=1).value.lower() for i in range(2, total_keyword_rows +
+                                                                          1)]
+for i in (3, 4):
+    common_keywords(i)
+print("Done")
+exit(0)
+
---
+++ README.md
@@ -0,0 +1,23 @@
+# Readme.md
+
+## Requirements
+1. git
+2. python3
+
+## Optional Steps
+
+  Create a directory
+  
+  Change into the directory
+  
+  Run `python3 -m venv <name-of-virtualenv>`
+  
+  Run `source <name-of-virtualenv>/bin/activate`
+  
+  To deactivate run `deactivate`
+
+## Steps  
+1. Run `git clone https://github.com/akhil-jha/execlreport.git`
+2. Run `python FinalReport.py`
+
+### NOTE: Only .xlsx file. 
