4eba38ba3a75a665d42144212439fca2322f0b1d
Akhil Jha <akjha@redhat.com>
2020-07-30 16:30:46 +0530

    Complete(I guess)

--- FinalReport.py
+++ FinalReport.py
@@ -17,8 +17,22 @@ def common_keywords(cal_val):
                 temp.append(keyword)
         temp = ",".join(temp)
         data.cell(row=i, column=cal_val + 3).value = temp
-    data.cell(row=1, column=8).value = "Common from F and G"
     content.save(file)
+    global flag
+    flag = flag + 1
+    if flag == 2:
+        data.cell(row=1, column=8).value = "Common from F and G"
+        for i in range(2, total_data_rows + 1):
+            problem = data.cell(row=i, column=6).value.split(",")
+            problem = set(problem)
+            original = data.cell(row=i, column=7).value.split(",")
+            original = set(original)
+            if problem & original:
+                temp = problem & original
+                temp = list(temp)
+                temp = ','.join(str(z) for z in temp)
+                data.cell(row=i, column=8).value = temp
+        content.save(file)
 
 
 def file_exists(file):
@@ -51,6 +65,8 @@ total_keyword_rows = components.max_row
 
 keywords = [components.cell(row=i, column=1).value.lower() for i in range(2, total_keyword_rows +
                                                                           1)]
+flag = 0
+
 for i in (3, 4):
     common_keywords(i)
 print("Done")
